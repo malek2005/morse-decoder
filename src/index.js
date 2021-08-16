4aec00fac1c4a5d69d59e307b8377417b5bf2ad0
malek2005 <malek2005@list.ru>
2021-08-16 12:03:04 +0300

    feat: solution for task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = []
+  for (let i = 0; i < expr.length / 10; i++) {
+    let numbers = expr.slice(i * 10, i * 10 + 10)
+    arr[i] = ''
+    for (let j = 0; j < 5; j++) {
+      switch (numbers.slice(j * 2, j * 2 + 2)) {
+        case "00" :
+          break;
+        case "**" :
+          arr[i] = ' ';
+          break;
+        case "10" :
+          arr[i] += '.';
+          break;
+        case "11":
+          arr[i] += '-';
+      }
+    }
+    arr[i] = MORSE_TABLE[arr[i]] || ' '
+  }
+  return arr.join('')
 }
 
 module.exports = {
